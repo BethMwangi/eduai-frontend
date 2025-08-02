0d3f5a80cbdb48d0e84006eadc078791c30fcabe
wanjikumwangib@gmail.com <wanjikumwangib@gmail.com>
2025-08-02 17:38:17 +0300

    change question pool view to exam view and paper view

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -146,10 +146,10 @@ export default function RegisterForm() {
     }
   };
 
-  const handleGoogleSignup = () => {
-    // Implement Google sign-in logic
-    userService.loginWithGoogle();
-  };
+  // const handleGoogleSignup = () => {
+  //   // Implement Google sign-in logic
+  //   userService.loginWithGoogle();
+  // };
   return (
     <div className="max-w-xl w-full space-y-8 bg-white p-10 rounded-lg shadow-lg">
       {/* Logo and Welcome Text */}
@@ -346,7 +346,7 @@ export default function RegisterForm() {
         </div>
 
         {/* Google Sign In Button */}
-        <button
+        {/* <button
           type="button"
           onClick={handleGoogleSignup}
           className="w-full flex items-center justify-center gap-2 bg-white border border-gray-300 text-gray-700 hover:bg-gray-50 py-3 px-4 rounded-lg font-medium transition"
@@ -358,7 +358,7 @@ export default function RegisterForm() {
             <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
           </svg>
           Sign up with Google
-        </button>
+        </button> */}
       </form>
 
       {/* Login Link */}
